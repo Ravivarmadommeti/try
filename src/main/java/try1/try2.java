87abbc8483a20b836c82e4ca53c9698370448182
Admin <Admin@DESKTOP-KLBDAVK>
2024-01-29 12:05:40 +0530

    done check once

--- src/main/java/try1/try2.java
+++ src/main/java/try1/try2.java
@@ -14,7 +14,10 @@ public class try2 {
 		Driver.findElement(By.id("pword")).sendKeys("ZPMHQHIA");
 		Thread.sleep(2000);
 		Driver.findElement(By.xpath("/html/body/div/div/div/div/div[2]/form/div[4]/button")).click();
-
+		Thread.sleep(2000);
+		Driver.findElement(By.className("rounded-circle")).click();
+//Driver.findElement(By.xpath("/html/body/div[3]/header/nav/div[2]/ul/li/div/div/a[2]")).click();
+Driver.findElement(By.xpath("//a[@class='dropdown-item'][2]")).click();
 	}
 
 }
